d1dd2486a5ec4edc8fc83d2395fd3cc57ef3877a
Rinwu1114 <Rinwu1114@gmail.com>
2025-08-25 02:32:14 -0500

    added navbar, started on homescreen

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar/>
+      <div className="hero">
+        <img src={hero_banner} alt="" className='banner__img'/>
+        <div className="hero__caption">
+            <img src={hero_title} alt="" className='caption__img'/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
