4d306610bf4805e2fd2b07577e57a0a200970919
Rinwu1114 <Rinwu1114@gmail.com>
2025-09-01 03:01:37 -0500

    added login page and sign up page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
+import logo from "../../assets/logo.png";
 
 const Login = () => {
-  return (
-    <div className='login'>
-      
-    </div>
-  )
-}
+  const [signState, setSignState] = useState("Sign In");
 
-export default Login
+  return (
+    <div className="login">
+      <img src={logo} className="login__logo" alt="" />
+      <div className="login__form">
+        <h1>{signState}</h1>
+        <form>
+          {signState === "Sign Up" ? (
+            <input type="text" placeholder="Username" />
+          ) : (
+            <></>
+          )}
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>{signState}</button>
+          <div className="form__help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="form__switch">
+          {signState === "Sign In" ? (
+            <p>
+              New to Netflix?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign Up")
+                }}
+              >
+                Sign Up
+              </span>
+            </p>
+          ) : (
+            <p>
+              Already have an account? <span onClick={() => {
+                  setSignState("Sign In")
+                }}>Sign In</span>
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
